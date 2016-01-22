de1a3ee386b14428793a2a186bc6d35d2c83ef22
phstearns <phstearns13@gmail.com>
2016-01-22 14:55:59 -0500

    Updated from Brython Server: 1/22/2016 2:55:59 PM

--- tutorial1.py
+++ tutorial1.py
@@ -1,3 +1,5 @@
-from ggame import App
+from ggame import App 
 myapp = App()
 myapp.run()
+from ggame import Color, LineStyle, Graphics
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
